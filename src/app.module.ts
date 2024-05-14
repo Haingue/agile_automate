1927da3127b56354c66a9fd36293086e784d08ca
haingue <fabien.haingue@gmail.com>
2024-05-14 22:12:00 +0200

    FEAT: add swagger and metrics

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,20 @@ import { ConfluenceModule } from './confluence/confluence.module';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrometheusModule } from '@willsoto/nestjs-prometheus';
 
 @Module({
-  imports: [JiraModule, TmmfModule, ConfluenceModule],
+  imports: [
+    JiraModule,
+    TmmfModule,
+    ConfluenceModule,
+    PrometheusModule.register({
+      defaultLabels: {
+        app: process.env.npm_package_name,
+        app_version: process.env.npm_package_version,
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { TmmfService } from './tmmf/tmmf.service';
-import { ConfluenceService } from './confluence/confluence.service';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Swagger configuration
+  const config = new DocumentBuilder()
+    .setTitle('Agile automate')
+    .setDescription(
+      'The Agile automate API to improve the user experience for Jira/Confluence',
+    )
+    .setVersion(process.env.npm_package_version)
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger-ui', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
