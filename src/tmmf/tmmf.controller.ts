d68c2d72fcda9d68c5fa3692c3004569a0c7f0ae
haingue <fabien.haingue@gmail.com>
2024-05-26 17:37:43 +0200

    FEAT: add endpoint to avoid mismatch between json structure (Jira/Confluence)

--- src/tmmf/tmmf.controller.ts
+++ src/tmmf/tmmf.controller.ts
@@ -2,7 +2,7 @@
 https://docs.nestjs.com/controllers#controllers
 */
 
-import { Body, Controller, Inject, Logger, Post } from '@nestjs/common';
+import { Body, Controller, Inject, Logger, Param, Post } from '@nestjs/common';
 import { Content } from 'src/confluence/types';
 import { TmmfService } from './tmmf.service';
 import { Issue } from 'src/jira/types';
@@ -16,16 +16,36 @@ export class TmmfController {
 
   @Post('approve-project')
   async approveProjectCanvas(@Body() canvasPage: Content): Promise<Issue> {
-    this.logger.log(`New project canva approve: ${canvasPage.title}`);
+    this.logger.log(`New project canvas approve: ${canvasPage.title}`);
     const initiative: Issue = await this.tmmfService.approveCanvas(canvasPage);
+    return initiative;
+  }
+
+  @Post('approve-project/:pageId')
+  async approveProjectCanvasByPageId(
+    @Param('pageId') pageId: number,
+  ): Promise<Issue> {
+    this.logger.log(`New project canvas approve by id: ${pageId}`);
+    const initiative: Issue =
+      await this.tmmfService.approveCanvasByPageId(pageId);
     return initiative;
   }
 
   @Post('start-project')
   putProjectInBacklog(@Body() inititativeIssue: Issue): Promise<any> {
-    this.logger.log(`New project canva approve: ${inititativeIssue.key}`);
+    this.logger.log(`Project stared: ${inititativeIssue.key}`);
     const result: Promise<any> =
       this.tmmfService.putProjectInBacklog(inititativeIssue);
+    return result;
+  }
+
+  @Post('start-project/:issueKey')
+  putProjectInBacklogByIssueKey(
+    @Param('issueKey') issueKey: string,
+  ): Promise<any> {
+    this.logger.log(`Project started by key: ${issueKey}`);
+    const result: Promise<any> =
+      this.tmmfService.putProjectInBacklogByIssueKey(issueKey);
     return result;
   }
 }
